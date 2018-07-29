4a129e2e8c7790e0cd179a1ab109fcf591744442
object88 <object88@gmail.com>
2018-07-29 14:24:53 -0600

    Unexport error code creation

--- frontend/pascal/token.go
+++ frontend/pascal/token.go
@@ -4,10 +4,12 @@ import (
 	"github.com/object88/writing-compilers-and-interpreters/frontend"
 )
 
+// Token is a pascal token
 type Token struct {
 	frontend.BaseToken
 }
 
+// NewToken returns a new pascal token from the source
 func NewToken(s *frontend.Source) *Token {
 	return &Token{
 		BaseToken: frontend.NewBaseToken(s),
